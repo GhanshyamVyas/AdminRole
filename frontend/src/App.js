afcdebaf1d453aed2f95b2a859e317e4fe299e73
vyasghanshyam <vyasghanshyamhvyas@gmail.com>
2024-04-27 14:51:54 +0530

    for new repo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [jokes, setJokes] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/owner/jokes")
+      .then((res) => {
+        // handle success
+        console.log(res.data);
+        setJokes(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>
+      <p>jokes : {jokes.length}</p>
+      {jokes.map((joke, id) => (
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>
+      ))}
     </div>
   );
 }
